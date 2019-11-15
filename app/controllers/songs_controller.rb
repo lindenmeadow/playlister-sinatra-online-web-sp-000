52e5172d7de0eddb871f2731c2b3858ca6fa5649
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-11-15 01:33:17 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,27 +5,11 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
-
     erb :'/songs/index'
   end
 
   get '/songs/new' do
-    @genres = Genre.all
-    @artists = Artist.all
-    erb :'songs/new'
-  end
-
-  post '/songs' do
-    @song = Song.find_or_create_by(name: params['song']['name'])
-
-    @artist = Artist.find_or_create_by(name: params['artist']['name'])
-    @song.artist_id = artist.id
-
-    @song.genre_ids = params["genres"]
-    @song.save
-    flash[:message] = "Successfully Created New Song!"
-
-    redirect "/songs/#{song.slug}"
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -34,34 +18,30 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
+  end
+
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @genres = Genre.all
     erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
-
     @song = Song.find_by_slug(params[:slug])
-
-    if @song.name != params['song']['name']
-      @song.update(name: params['song']['name'])
-      @song.name = Song.find_or_create_by(name: params[:song][:name])
-    end
     @song.update(params[:song])
-
-    if @song.artist.name != params['artist']['name']
-       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    end
-
-
-    if @song.genre_ids != params['genres']
-       @song.genre_ids = params['genres']
-    end
-
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
     @song.save
-    flash[:message] = "Successfully Updated Song"
-    redirect "/songs/#{@song.slug}"
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,10 +1,14 @@
-require_relative "./concerns/slugifiable.rb"
-
 class Genre < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
-
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    name.downcase.tr(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    Genre.all.find { |genre| genre.slug == slug }
+  end
+
 end
